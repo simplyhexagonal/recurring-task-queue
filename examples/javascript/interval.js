c9b243f2bd31be6027c110a66a8962b1c8ae9024
Jean M. Lescure <jeanmlescure@gmail.com>
2021-10-11 22:21:03 -0600

    chore: add ts and js examples

---
+++ examples/javascript/interval.js
@@ -0,0 +1,93 @@
+const { createServer, IncomingMessage, ServerResponse } = require('http');
+
+const RTQ = require('../../dist/recurring-task-queue');
+
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+const helloTask = async (taskOptions) => {
+  await sleep(2000);
+  console.log(`
+  👋 hello ${taskOptions.arrivingUser}
+  `);
+};
+
+const goodbyeTask = async (taskOptions) => {
+  await sleep(2000);
+  console.log(`
+  🚀 goodbye ${taskOptions.departingUser}
+  `);
+};
+
+const tasks = [
+  {
+    id: '1',
+    status: 'NEW',
+    waitTimeBetweenRuns: 6000,
+    taskName: 'hello',
+    maxRetries: 1,
+    retryCount: 0,
+    lastRun: new Date(0),
+    taskOptions: {
+      arrivingUser: 'you',
+    },
+  },
+  {
+    id: '2',
+    status: 'NEW',
+    waitTimeBetweenRuns: 6000,
+    taskName: 'goodbye',
+    maxRetries: 1,
+    retryCount: 0,
+    lastRun: new Date(0),
+    taskOptions: {
+      departingUser: 'you',
+    },
+  }
+];
+
+const queue = [];
+
+const fetchTasks = async () => tasks;
+
+const updateTask = async (task) => {
+  const i = tasks.findIndex((t) => t.id === task.id);
+
+  tasks[i] = task;
+};
+
+const createQueueEntry = async (queueEntry) => {
+  queue.push(queueEntry);
+};
+
+const fetchQueueEntries = async () => queue;
+
+const removeQueueEntry = async (queueEntry) => {
+  const i = queue.findIndex((qe) => qe.id === queueEntry.id);
+  if (i > -1) {
+    queue.splice(i, 1);
+  } else {
+    throw new Error();
+  }
+};
+
+const logAction = async (logEntry) => {
+  console.info(logEntry);
+};
+
+const taskHandlers = {
+  hello: helloTask,
+  goodbye: goodbyeTask,
+};
+
+const recurring = new RTQ({
+  fetchTasks,
+  updateTask,
+  createQueueEntry,
+  fetchQueueEntries,
+  removeQueueEntry,
+  logAction,
+  taskHandlers,
+});
+
+console.log('About to start...');
+setInterval(() => recurring.tick(), 1000);
---
+++ examples/typescript/server.ts
@@ -0,0 +1,118 @@
+import { createServer, IncomingMessage, ServerResponse } from 'http';
+
+import RTQ, {
+  RTQLogEntry,
+  RTQQueueEntry,
+  RTQStatus,
+  RTQTask,
+  RTQTaskHandler,
+} from '../../dist/recurring-task-queue';
+
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+interface helloTaskOptions {
+  arrivingUser: string;
+}
+
+const helloTask: RTQTaskHandler<helloTaskOptions> = async (taskOptions) => {
+  await sleep(2000);
+  console.log(`
+  👋 hello ${taskOptions.arrivingUser}
+  `);
+};
+
+interface goodbyeTaskOptions {
+  departingUser: string;
+}
+
+const goodbyeTask: RTQTaskHandler<goodbyeTaskOptions> = async (taskOptions) => {
+  await sleep(2000);
+  console.log(`
+  🚀 goodbye ${taskOptions.departingUser}
+  `);
+};
+
+type allTaskOptions = helloTaskOptions | goodbyeTaskOptions | any;
+
+const tasks: RTQTask<allTaskOptions>[] = [
+  {
+    id: '1',
+    status: RTQStatus.NEW,
+    waitTimeBetweenRuns: 6000,
+    taskName: 'hello',
+    maxRetries: 1,
+    retryCount: 0,
+    lastRun: new Date(0),
+    taskOptions: {
+      arrivingUser: 'you',
+    },
+  },
+  {
+    id: '2',
+    status: RTQStatus.NEW,
+    waitTimeBetweenRuns: 6000,
+    taskName: 'goodbye',
+    maxRetries: 1,
+    retryCount: 0,
+    lastRun: new Date(0),
+    taskOptions: {
+      departingUser: 'you',
+    },
+  }
+];
+
+const queue: RTQQueueEntry[] = [];
+
+const fetchTasks = async () => tasks;
+
+const updateTask = async (task: RTQTask<allTaskOptions>) => {
+  const i = tasks.findIndex((t) => t.id === task.id);
+
+  tasks[i] = task;
+};
+
+const createQueueEntry = async (queueEntry: RTQQueueEntry) => {
+  queue.push(queueEntry);
+};
+
+const fetchQueueEntries = async () => queue;
+
+const removeQueueEntry = async (queueEntry: RTQQueueEntry) => {
+  const i = queue.findIndex((qe) => qe.id === queueEntry.id);
+  if (i > -1) {
+    queue.splice(i, 1);
+  } else {
+    throw new Error();
+  }
+};
+
+const logAction = async (logEntry: RTQLogEntry) => {
+  console.info(logEntry);
+};
+
+const taskHandlers: {[k: string]: RTQTaskHandler<allTaskOptions>} = {
+  hello: helloTask,
+  goodbye: goodbyeTask,
+};
+
+const recurring = new RTQ({
+  fetchTasks,
+  updateTask,
+  createQueueEntry,
+  fetchQueueEntries,
+  removeQueueEntry,
+  logAction,
+  taskHandlers,
+});
+
+const port = 5000;
+
+const server = createServer((request: IncomingMessage, response: ServerResponse) => {
+  recurring.tick();
+
+  response.end('Running tasks...');
+});
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
